d8b3ee5fbe020b2571d56c03b223e13704f941e5
stivenduque09 <133989681+stivenduque09@users.noreply.github.com>
2026-06-25 17:07:29 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="EcoCom2 Circular IA",
+    page_icon="♻️"
+)
+
+st.title("♻️ EcoCom2 Circular IA")
+
+st.write(
+    "Sistema inteligente de gestión de residuos."
+)
+
+st.success("Aplicación en construcción.")
